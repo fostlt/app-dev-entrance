cfbdce0e5994d424b4ac5960bbb1770bbaf242ad
fostlt <lanceadamp@gmail.com>
2020-01-11 15:32:57 -0800

    fix game loop

--- js/scripts.js
+++ js/scripts.js
@@ -10,28 +10,32 @@ const game = {};
 
 quizOne.addEventListener('click', loadQuestions);
 function loadQuestions(){
-  
+  quizOne.style.display = "none";
   fetch(url).then(function(res){
     return res.json()
   }).then(function(data){
     console.log(data);
-    
     game.total = data.quizzes[0].questions.length;
     game.val = 0;
     game.score = 0;
     game.arr = data.quizzes[0].questions;
     game.ans = data.quizzes[0].questions.answers;
+    game.test = 0;
+    let number = 0 + Math.floor(Math.random() * 3);
     console.log(game.ans);
 
     console.log(typeof game.score);
-    
-    message.textContent = 'Question #'+(game.val+1)+ ' out of '+ (game.total)
+
+
+
+
+    //message.textContent = 'Question #'+(game.val+1)+ ' out of '+ (game.total)
     question.innerHTML = '';
-    let q = game.arr[game.val].answers;
+    let q = game.arr[number].answers;
     console.log(q);
     const main = document.createElement('div');
     const theQuestion = document.createElement('div');
-    theQuestion.textContent = game.arr[game.val].question;
+    theQuestion.textContent = game.arr[number].question;
     message.appendChild(theQuestion);
     q.forEach(function(el){
       console.log(el);
@@ -50,28 +54,39 @@ function loadQuestions(){
           ele.removeEventListener('click', checker);
         })
 
-        if (span.answer == true){
+        if (span.answer === true){
           console.log("nice");  
           span.style.color = 'green';
           next.textContent = 'Correct';
           game.score++;
+         
           
         }else{
           console.log("not nice");
           span.style.color = 'red';
           next.textContent = 'Incorrect';
-
         }
         next.style.display = "block";
-        console.log(game.val);
         next.addEventListener('click', newAnswer);
         function newAnswer(){
+          console.log("test");
+          //next.removeEventListener('click', newAnswer);
+          message.innerHTML = "";
           game.val++;
-          message.innerHTML = '';
-          console.log(game.val);
-          console.log('score is '+ game.score);
+          next.style.display = "none";
+          quizOne.style.display = "block";
+          quizOne.textContent = "Next Question";
+          //console.log(rndmNumber + 'tst');
+          console.log(game);
+          // if (game.val === 3){
+          //   message.textContent = 'Your score was ' + game.score;
+          // }
+
+
+          
+
+
         }
-        
        
 
       }
@@ -82,11 +97,19 @@ function loadQuestions(){
   
 }
 
-// function loadQuestionsOne(){
-//   message.textContent = 'Question #'+(game.val+1)+ ' out of '+ (game.total)
-//   question.innerHTML = '';
-  
-// }
+
+
+
+
+        //console.log(game.val);
+
+        // function newAnswer(){
+        //   rndmNumber++
+        //   message.innerHTML = '';
+        //   loadQuestions();
+        //   next.style.display = "none";
+        //   //console.log(gameAdd + 'gamescore');
+        // }
 
 
 
